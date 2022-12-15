0121e0c9467d2783c46e0c4d0ef9716b42460dd9
Cédric <93099582+cedb13@users.noreply.github.com>
2022-12-15 06:56:43 +0100

    Update public/js/scripts.js
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -16,7 +16,7 @@ window.addEventListener('DOMContentLoaded', () => {
             } else {
                 console.log(123);
                 mainNav.classList.remove('is-visible', 'is-fixed');
-            }
+      }
         } else {
             // Scrolling Down
             mainNav.classList.remove(['is-visible']);
