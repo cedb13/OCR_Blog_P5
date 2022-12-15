18f43a79cf60af2cb21be70248219650365ed4c9
Cédric Bonche <cedric.bonche@gmail.com>
2022-12-15 02:47:22 +0100

    Prepare files from folder to Issue-1- Init_Framework_MVC

---
+++ App/Lib/Database.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Lib;
+
+use \PDO;
+
+class Database{
+
+    private $db_name;
+    private $db_user;
+    private $db_pass;
+    private $db_host;
+    private $pdo;
+
+    public function __construct($db_name = 'blog_pro_p5', $db_user = 'root', $db_pass ='#@root@#', $db_host = 'localhost'){
+        $this->db_name = $db_name;
+        $this->db_user = $db_user;
+        $this->db_pass = $db_pass;
+        $this->db_host = $db_host;
+    }
+
+    private function getPDO(){
+        if($this->pdo === null){
+            $pdo = new PDO('mysql:dbname=blog_pro_p5;host=localhost', 'root', '#@root@#');
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->pdo = $pdo;
+        }
+        return $this->pdo;
+    }
+
+    public function query($statement, $class_name, $one = false){
+        $req = $this->getPDO()->query($statement);
+        $req->setFetchMode(PDO::FETCH_CLASS, $class_name);
+        if($one){
+            $datas = $req->fetch();
+        } else {
+            $datas = $req->fetchAll();
+        }
+        return $datas;
+    }
+
+    public function prepare($statement, $attributes, $class_name, $one = false){
+        $req = $this->getPDO()->prepare($statement);
+        $req->execute($attributes);
+        $req->setFetchMode(PDO::FETCH_CLASS, $class_name);
+        if($one){
+            $datas = $req->fetch();
+        } else {
+            $datas = $req->fetchAll();
+        }
+        return $datas;
+    }
+
+}
