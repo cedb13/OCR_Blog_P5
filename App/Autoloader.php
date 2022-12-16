7b3126e5bb66c396bdeaa97c78728289ea660cc5
Cédric Bonche <cedric.bonche@gmail.com>
2022-12-16 01:52:24 +0100

    Add files Home for the model, home for the view, HomeController, complete file: - in model folders database file, App file and Table file for the connection  and recover element from database and prepare model for controllers. - in App folder  Autoloader and htaccess files. - in public folder index file.

---
+++ App/Autoloader.php
@@ -0,0 +1,30 @@
+<?php
+namespace App;
+
+/**
+ * Class Autoloader
+ * @package App
+ */
+class Autoloader{
+
+    /**
+     * Enregistre notre autoloader
+     */
+    static function register(){
+        spl_autoload_register(array(__CLASS__,'autoload'));
+    }
+
+    /**
+     * Inclue le fichier correspondant à notre classes
+     * @param $class string le nom de la classe à charger
+     */
+    static function autoload($class){
+        if (strpos($class, __NAMESPACE__ . '\\') === 0){
+            $class = str_replace (__NAMESPACE__.'\\', '', $class);
+            $class = str_replace('\\', '/', $class);
+            //echo __DIR__ .'/'.$class.'.php';
+            //echo '<br>';
+            require __DIR__ .'/'.$class.'.php';
+        }
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+define('ROOT', dirname(__DIR__));
+
+require_once ROOT.'/App/Autoloader.php';
+App\Autoloader::register();
+
+$page='home';
+$action='show';
+
+if(isset($_GET['page'])){
+    $page = $_GET['page'];
+}
+
+if(isset($_GET['action'])){
+    $action = $_GET['action'];
+}
+
+
+$nameController="App\Controllers\\".ucfirst($page)."Controller";
+
+if(!class_exists($nameController) || !method_exists($nameController, $action)){
+    header('Location: http://localhost/OCR_Blog_P5/public/');
+ }
+
+$controller = new $nameController;
+$controller->$action();
+
+
+
