2d844cac36820b58388c8b67b4b50db79f8056d1
Cédric Bonche <cedric.bonche@gmail.com>
2023-04-11 13:35:35 +0200

    upload cleaning

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -34,75 +34,6 @@ class HomeController extends Controller{
     }
 
     public function contactForm(){
-       /* if (isset($_POST)) {
-            $to             = "oldced.devtest@gmail.com";
-            $subject        = htmlspecialchars($_POST['subject']);
-            $message        = htmlspecialchars($_POST['message']);
-            $emailSanitize	= filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-            $email			= filter_var($emailSanitize, FILTER_VALIDATE_EMAIL);
-
-            $com['com'] = array('com1'=>'Le blog Dev de Cédric','com2'=>'Votre message a bien été envoyé');
-
-            $headers  = 'MIME-Version: 1.0'."\r\n";
-            $headers .= 'Content-type: text/html; charset=utf-8'."\r\n";
-            $headers .= 'From: oldced.devtest@gmail.com'."\r\n";
-            $headers .= 'Reply-to: '.$email;
-
-            $message = '<h1>Message envoyé depuis la page Contact de leblogdevdeced</h1>
-            <p><b>Email : </b>'.$email.'<br>
-            <b>Message : </b>'.$message.'</p>'; */
-
-            /* echo "<pre>";
-            var_dump($message);
-            echo "<pre>";
-            die;*/
-
-           /* if(!mail($to, $subject, $message, $headers)){
-                header('Location:http://localhost/OCR_Blog_P5/public/index.php?page=home#contact');
-            }
-            else{
-
-                $posts['posts'] = $this->postService->getAllPost();
-                $lastPosts['lastPosts'] = $this->postService->getLastPost();
-                $contents['cv']= array ('nom'=>'Bonche', 'prenom'=>'Cédric');
-                $this->setContents($com);
-                $this->setContents($posts);
-                $this->setContents($lastPosts);
-                $this->setContents($contents);
-                $this->render('home');
-            }   
-        }*/
-
-        /*    $to             = "idella.glover97@ethereal.email";
-            $subject        = "test";
-            $message        = "Hello";
-
-            $headers  = 'MIME-Version: 1.0'."\r\n";
-            $headers .= 'Content-type: text/html; charset=utf-8'."\r\n";
-            $headers .= 'From: idella.glover97@ethereal.email'."\r\n";*/
-
-
-
-            /* echo "<pre>";
-            var_dump($message);
-            echo "<pre>";
-            die;*/
-
-           /* if(mb_send_mail($to, $subject, $message, $headers)){
-                $com['com'] = array('com1'=>'Le blog Dev de Cédric','com2'=>'Votre message a bien été envoyé');
-			}
-			else{
-                $com['com'] = array('com1'=>'Le blog Dev de Cédric','com2'=>'Erreur envoi');
-			}
-
-            $posts['posts'] = $this->postService->getAllPost();
-            $lastPosts['lastPosts'] = $this->postService->getLastPost();
-            $contents['cv']= array ('nom'=>'Bonche', 'prenom'=>'Cédric');
-            $this->setContents($com);
-            $this->setContents($posts);
-            $this->setContents($lastPosts);
-            $this->setContents($contents);
-            $this->render('home');*/
             
             $send = empty($send);
             if (!empty($_POST['lastName']) && !empty($_POST['firstName']) && !empty($_POST['email']) && !empty($_POST['subject']) && !empty($_POST['message']) ) {
@@ -114,7 +45,7 @@ class HomeController extends Controller{
                 $subject        = htmlspecialchars($_POST['subject']);
                 $message        = htmlspecialchars($_POST['message']);
 
-                $send = $register=$this->sendService->sendMail($subject, $email, $message);
+                $send = $this->sendService->sendMail($subject, $email, $message);
                 $com['com'] = array('com1'=>'Le blog Dev de Cédric','com2'=>'Votre message a bien été envoyé');
                 $posts['posts'] = $this->postService->getAllPost();
                 $lastPosts['lastPosts'] = $this->postService->getLastPost();
@@ -128,7 +59,6 @@ class HomeController extends Controller{
             else{
                 $com['com'] = array('com1'=>'Le blog Dev de Cédric','com2'=>'Erreur envoi');
                 header('Location:http://localhost/OCR_Blog_P5/public/index.php?page=home#contact');
-                die();
             }
     }
 }
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,21 +7,21 @@ window.addEventListener("DOMContentLoaded", () => {
     let scrollPos = 0;
   const mainNav = document.getElementById("mainNav");
     const headerHeight = mainNav.clientHeight;
-    window.addEventListener('scroll', function() {
+    window.addEventListener("scroll", function() {
         const currentTop = document.body.getBoundingClientRect().top * -1;
         if ( currentTop < scrollPos) {
             // Scrolling Up
-            if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
-                mainNav.classList.add('is-visible');
+            if (currentTop > 0 && mainNav.classList.contains("is-fixed")) {
+                mainNav.classList.add("is-visible");
             } else {
                 console.log(123);
-                mainNav.classList.remove('is-visible', 'is-fixed');
+                mainNav.classList.remove("is-visible", "is-fixed");
       }
         } else {
             // Scrolling Down
-            mainNav.classList.remove(['is-visible']);
-            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
-                mainNav.classList.add('is-fixed');
+            mainNav.classList.remove(["is-visible"]);
+            if (currentTop > headerHeight && !mainNav.classList.contains("is-fixed")) {
+                mainNav.classList.add("is-fixed");
             }
         }
         scrollPos = currentTop;
